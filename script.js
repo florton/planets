cf165bef7ba670f85f986c7828efc77cfe13d62e
Flanders <flanders.lorton@gmail.com>
2023-09-19 02:58:08 -0700

    wip

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 import * as THREE from "three"
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
-import { RenderPass } from "/node_modules/three/examples/jsm/postprocessing/RenderPass.js";
-import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import { SelectiveBloomEffect, EffectComposer, EffectPass, RenderPass, BlendFunction, FXAAEffect } from "postprocessing";
+// import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
+// import { EffectComposer as EffectComposer2 } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
 
 //Scene
 const scene = new THREE.Scene()
 
 const planets = {}
+const bloomObjs = []
 
-const addPlanet = (name, color, radius, x, y, z, layer=0) => {
+const addPlanet = (name, color, radius, x, y, z) => {
   //Create our sphere
   // const geometry = new THREE.SphereGeometry(radius, 64, 64)
   const geometry = new THREE.IcosahedronGeometry(radius, 15);
@@ -18,13 +19,16 @@ const addPlanet = (name, color, radius, x, y, z, layer=0) => {
   const mesh = new THREE.Mesh (geometry, material)
   mesh.position.set(x, y, z)
   planets[name] = mesh
-  mesh.layers.set(layer)
   scene.add(mesh)
+  return mesh
 }
 
 //Planets
-addPlanet("sun","#FDB813", 4, 0, 0, 0, 1)
-addPlanet("fakesun","#FDB813", 4, 0, 0, 0, 2)
+const sun = addPlanet("sun","#ffff00", 4, 0, 0, 0)
+bloomObjs.push(sun)
+
+// const fakesun = addPlanet("sun","#FDB813", 4, 0, 0, 0, true)
+// fakesun.layers.set(2)
 
 addPlanet("green","#00ff83", 3, 20, 10, 10)
 addPlanet("red","#880033", 2.5, 45, 20, 20)
@@ -32,16 +36,15 @@ addPlanet("blue","#220099", 5, 60, 40, 40)
 
 // Lights
 const pointLight = new THREE.PointLight(0xffffff, 10, 0, 0.1); 
-const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(pointLight, ambientLight);
 
-const ambientLightSun = new THREE.AmbientLight(0xffffff, 1);
-ambientLight.layers.set(1)
+const ambientLightSun = new THREE.AmbientLight(0xffffff, 10);
 scene.add(ambientLightSun)
 
-const ambientLightFakeSun = new THREE.AmbientLight(0xffffff, 100);
-ambientLightFakeSun.layers.set(2)
-scene.add(ambientLightFakeSun)
+// const ambientFakeLightSun = new THREE.AmbientLight(0xffffff, 10);
+// bloomObjs.push(ambientFakeLightSun)
+// scene.add(ambientFakeLightSun)
 
 
 //Camera
@@ -63,33 +66,48 @@ renderer.autoClear = false;
 //bloom renderer
 const renderScene = new RenderPass(scene, camera)
 
-const bloomPass = new UnrealBloomPass(
-  new THREE.Vector2(window.innerWidth, window.innerHeight)
-)
-bloomPass.threshold = 0
-bloomPass.strength = 5 //intensity of glow
-bloomPass.radius = 1
+const selectiveBloom = new SelectiveBloomEffect(scene, camera, {
+  blendFunction: BlendFunction.AVERAGE,
+  intensity:10,
+  // luminanceThreshold: 0.0001,
+  // luminanceSmoothing: 0.1,
+  // mipmapBlur: true,
+  // radius: 0.4,
+  // levels: 5,
+})
+selectiveBloom.selection = bloomObjs
+const bloomPass = new EffectPass(camera, selectiveBloom )
+
+// const fxaaPass = new EffectPass(camera, new FXAAEffect())
 
 const composer = new EffectComposer(renderer)
-composer.setSize(window.innerWidth, window.innerHeight)
-// bloomComposer.renderToScreen = true
 composer.addPass(renderScene)
 composer.addPass(bloomPass)
+// composer.addPass(fxaaPass)
+
+// const unrealBloomPass = new UnrealBloomPass(
+//   new THREE.Vector2(window.innerWidth, window.innerHeight),1,1,1
+// )
+// unrealBloomPass.threshold = 0
+// unrealBloomPass.strength = 1 //intensity of glow
+// unrealBloomPass.radius = 1
+// const unrealComposer = new EffectComposer2(renderer)
+// unrealComposer.setSize(window.innerWidth, window.innerHeight)
+// // unrealComposer.renderToScreen = true
+// unrealComposer.addPass(renderScene)
+// unrealComposer.addPass(unrealBloomPass)
 
 
 function render() {
   renderer.clear();
+  // unrealComposer.render()
   
-  camera.layers.set(1);
+  // camera.layers.set(0);
   composer.render();
   
-  renderer.clearDepth();
-  camera.layers.set(0);
-  renderer.render(scene, camera);
-  
-  renderer.clearDepth();
-  camera.layers.set(2);
-  renderer.render(scene, camera);
+  // renderer.clearDepth();
+  // camera.layers.set(0);
+  // renderer.render(scene, camera);
   
   // requestAnimationFrame(render);
 }
