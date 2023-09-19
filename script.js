4b36892fc409b268304c7488a31e12dc05dc00c8
Flanders <flanders.lorton@gmail.com>
2023-09-19 01:30:56 -0700

    almost 2

--- script.js
+++ script.js
@@ -6,43 +6,49 @@ import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing
 
 //Scene
 const scene = new THREE.Scene()
-const sunScene = new THREE.Scene()
 
 const planets = {}
 
-const addPlanet = (name, color, radius, x, y, z, s = scene) => {
+const addPlanet = (name, color, radius, x, y, z, layer=0) => {
   //Create our sphere
   // const geometry = new THREE.SphereGeometry(radius, 64, 64)
   const geometry = new THREE.IcosahedronGeometry(radius, 15);
   let material = null
-  material = new THREE.MeshStandardMaterial({ color, roughness: 0.5 })
+  material = new THREE.MeshStandardMaterial({ color, roughness: 0.5, metalness: 0.5 })
   const mesh = new THREE.Mesh (geometry, material)
   mesh.position.set(x, y, z)
   planets[name] = mesh
-  s.add(mesh)
+  mesh.layers.set(layer)
+  scene.add(mesh)
 }
 
 //Planets
-addPlanet("sun","#FDB813", 4, 0, 0, 0, sunScene)
+addPlanet("sun","#FDB813", 4, 0, 0, 0, 1)
+addPlanet("fakesun","#FDB813", 4, 0, 0, 0, 2)
 
 addPlanet("green","#00ff83", 3, 20, 10, 10)
 addPlanet("red","#880033", 2.5, 45, 20, 20)
 addPlanet("blue","#220099", 5, 60, 40, 40)
 
 // Lights
-const pointLight = new THREE.PointLight(0xffffff, 10, 0, 0.1);  
-// pointLight.position.set(20,20,0);
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+const pointLight = new THREE.PointLight(0xffffff, 10, 0, 0.1); 
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
 scene.add(pointLight, ambientLight);
 
-const ambientLightSun = new THREE.AmbientLight(0xffffff, 10);
-sunScene.add(ambientLightSun)
+const ambientLightSun = new THREE.AmbientLight(0xffffff, 1);
+ambientLight.layers.set(1)
+scene.add(ambientLightSun)
+
+const ambientLightFakeSun = new THREE.AmbientLight(0xffffff, 100);
+ambientLightFakeSun.layers.set(2)
+scene.add(ambientLightFakeSun)
 
 
 //Camera
 const camera = new THREE.PerspectiveCamera (45, 800 / 600)
 camera.position.z = 100
+// camera.layers.enable(1);
+// camera.layers.enable(2);
 
 //Renderer
 const canvas = document.querySelector(".webgl")
@@ -55,34 +61,37 @@ renderer.autoClear = false;
 // camera.updateProjectionMatrix()
 
 //bloom renderer
-// const renderScene = new RenderPass(scene, camera)
-// const renderSunScene = new RenderPass(sunScene, camera)
-// const bloomPass = new UnrealBloomPass(
-//   new THREE.Vector2(window.innerWidth, window.innerHeight),
-//   1.5,
-//   0.4,
-//   0.85
-// )
-// bloomPass.threshold = 0
-// bloomPass.strength = 1 //intensity of glow
-// bloomPass.radius = 0
-// const bloomComposer = new EffectComposer(renderer)
-// bloomComposer.setSize(window.innerWidth, window.innerHeight)
-// // bloomComposer.renderToScreen = true
-// bloomComposer.addPass(renderSunScene)
-// bloomComposer.addPass(bloomPass)
+const renderScene = new RenderPass(scene, camera)
 
-// //final composer
-// const finalComposer = new EffectComposer( renderer );
-// finalComposer.addPass( renderScene );
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight)
+)
+bloomPass.threshold = 0
+bloomPass.strength = 5 //intensity of glow
+bloomPass.radius = 1
+
+const composer = new EffectComposer(renderer)
+composer.setSize(window.innerWidth, window.innerHeight)
+// bloomComposer.renderToScreen = true
+composer.addPass(renderScene)
+composer.addPass(bloomPass)
+
 
 function render() {
-  renderer.clear()
-  renderer.render( sunScene, camera )
-  // bloomComposer.render()
-  // gl.clearDepth()
-  renderer.render( scene, camera )
-  // finalComposer.render()
+  renderer.clear();
+  
+  camera.layers.set(1);
+  composer.render();
+  
+  renderer.clearDepth();
+  camera.layers.set(0);
+  renderer.render(scene, camera);
+  
+  renderer.clearDepth();
+  camera.layers.set(2);
+  renderer.render(scene, camera);
+  
+  // requestAnimationFrame(render);
 }
 
 
