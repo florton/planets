0535c28b235c1e2a605a7d0118328cd346a90267
Flanders <flanders.lorton@gmail.com>
2023-09-19 12:23:17 -0700

    sun working

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 import * as THREE from "three"
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { SelectiveBloomEffect, EffectComposer, EffectPass, RenderPass, BlendFunction, FXAAEffect } from "postprocessing";
-// import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
-// import { EffectComposer as EffectComposer2 } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
+// import { BloomEffect, SelectiveBloomEffect, EffectComposer, EffectPass, RenderPass, BlendFunction, FXAAEffect } from "postprocessing";
+import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
+import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 
 //Scene
 const scene = new THREE.Scene()
@@ -10,13 +13,22 @@ const scene = new THREE.Scene()
 const planets = {}
 const bloomObjs = []
 
-const addPlanet = (name, color, radius, x, y, z) => {
+const addPlanet = (name, color, radius, x, y, z, bloom = false) => {
   //Create our sphere
   // const geometry = new THREE.SphereGeometry(radius, 64, 64)
   const geometry = new THREE.IcosahedronGeometry(radius, 15);
   let material = null
-  material = new THREE.MeshStandardMaterial({ color, roughness: 0.5, metalness: 0.5 })
-  const mesh = new THREE.Mesh (geometry, material)
+  if (bloom) {
+    material = new THREE.MeshStandardMaterial({ color, roughness: 0.5, metalness: 0.5, emissive:"orange", emissiveIntensity:2.5, toneMapped:false})
+  } else {
+    const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+    const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+    material = new THREE.MeshStandardMaterial({ color, roughness: 0.5, metalness: 0.5, map: moonTexture, normalMap: normalTexture})
+  }
+
+
+  // material = new THREE.MeshStandardMaterial({ color: "red" })
+  const mesh = new THREE.Mesh(geometry, material)
   mesh.position.set(x, y, z)
   planets[name] = mesh
   scene.add(mesh)
@@ -24,22 +36,25 @@ const addPlanet = (name, color, radius, x, y, z) => {
 }
 
 //Planets
-const sun = addPlanet("sun","#ffff00", 4, 0, 0, 0)
+const sun = addPlanet("sun","#ffff66", 4, 0, 0, 0, true)
+// sun.layers.set(1)
 bloomObjs.push(sun)
 
 // const fakesun = addPlanet("sun","#FDB813", 4, 0, 0, 0, true)
 // fakesun.layers.set(2)
 
-addPlanet("green","#00ff83", 3, 20, 10, 10)
+addPlanet("green","#00ff83", 3, 30, 10, 10)
 addPlanet("red","#880033", 2.5, 45, 20, 20)
 addPlanet("blue","#220099", 5, 60, 40, 40)
 
 // Lights
 const pointLight = new THREE.PointLight(0xffffff, 10, 0, 0.1); 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(pointLight, ambientLight);
+scene.add(pointLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+// scene.add(ambientLight);
 
-const ambientLightSun = new THREE.AmbientLight(0xffffff, 10);
+const ambientLightSun = new THREE.AmbientLight(0xffffff, 1);
+// ambientLightSun.layers.set(1)
 scene.add(ambientLightSun)
 
 // const ambientFakeLightSun = new THREE.AmbientLight(0xffffff, 10);
@@ -56,60 +71,82 @@ camera.position.z = 100
 //Renderer
 const canvas = document.querySelector(".webgl")
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
-renderer.setSize(1200, 900)
+// renderer.setSize(1200, 900)
 renderer.autoClear = false;
-// renderer.setSize(window.innerWidth, window.innerHeight)
-// renderer.setPixelRatio(window.devicePixelRatio)
-// camera.aspect = width / height
-// camera.updateProjectionMatrix()
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+camera.aspect = window.innerWidth / window.innerHeight
+camera.updateProjectionMatrix()
 
 //bloom renderer
 const renderScene = new RenderPass(scene, camera)
 
-const selectiveBloom = new SelectiveBloomEffect(scene, camera, {
-  blendFunction: BlendFunction.AVERAGE,
-  intensity:10,
-  // luminanceThreshold: 0.0001,
-  // luminanceSmoothing: 0.1,
-  // mipmapBlur: true,
-  // radius: 0.4,
-  // levels: 5,
-})
-selectiveBloom.selection = bloomObjs
-const bloomPass = new EffectPass(camera, selectiveBloom )
+// const selectiveBloom = new SelectiveBloomEffect(scene, camera, {
+//   blendFunction: BlendFunction.AVERAGE,
+//   intensity:10,
+//   luminanceThreshold: 0.0001,
+//   luminanceSmoothing: 0.1,
+//   mipmapBlur: true,
+//   radius: 0.4,
+//   levels: 5,
+// })
+// selectiveBloom.selection = bloomObjs
+// console.log(bloomObjs)
+// const selBloomPass = new EffectPass(camera, selectiveBloom )
 
 // const fxaaPass = new EffectPass(camera, new FXAAEffect())
 
-const composer = new EffectComposer(renderer)
-composer.addPass(renderScene)
-composer.addPass(bloomPass)
+// const composer = new EffectComposer(renderer)
+// composer.addPass(renderScene)
+// composer.addPass(selBloomPass)
+// composer.addPass(bloomPass)
 // composer.addPass(fxaaPass)
 
-// const unrealBloomPass = new UnrealBloomPass(
-//   new THREE.Vector2(window.innerWidth, window.innerHeight),1,1,1
-// )
-// unrealBloomPass.threshold = 0
-// unrealBloomPass.strength = 1 //intensity of glow
-// unrealBloomPass.radius = 1
-// const unrealComposer = new EffectComposer2(renderer)
-// unrealComposer.setSize(window.innerWidth, window.innerHeight)
-// // unrealComposer.renderToScreen = true
-// unrealComposer.addPass(renderScene)
-// unrealComposer.addPass(unrealBloomPass)
+const unrealBloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),1.5,1,1
+)
+
+// const bloomComposer = new EffectComposer( renderer );
+// bloomComposer.renderToScreen = false;
+// bloomComposer.addPass( renderScene );
+// bloomComposer.addPass( unrealBloomPass );
+
+// const mixPass = new ShaderPass(
+//   new THREE.ShaderMaterial( {
+//     uniforms: {
+//       baseTexture: { value: null },
+//       bloomTexture: { value: bloomComposer.renderTarget2.texture }
+//     },
+//     // vertexShader: document.getElementById( 'vertexshader' ).textContent,
+//     // fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+//     defines: {}
+//   } ), 'baseTexture'
+// );
+// mixPass.needsSwap = true;
+
+// const outputPass = new OutputPass();
+
+const unrealComposer = new EffectComposer(renderer)
+unrealComposer.setSize(window.innerWidth, window.innerHeight)
+// unrealComposer.renderToScreen = true
+unrealComposer.addPass(renderScene)
+unrealComposer.addPass(unrealBloomPass)
+// unrealComposer.addPass(mixPass)
+// unrealComposer.addPass(outputPass)
 
 
 function render() {
   renderer.clear();
-  // unrealComposer.render()
+  unrealComposer.render()
   
   // camera.layers.set(0);
-  composer.render();
+  // composer.render();
   
   // renderer.clearDepth();
   // camera.layers.set(0);
   // renderer.render(scene, camera);
   
-  // requestAnimationFrame(render);
+  requestAnimationFrame(render);
 }
 
 
